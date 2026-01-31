3e3c3fdd9a222e1e01430e836bb1595de709a494
amelialo <amelialo@utexas.edu>
2026-01-31 02:57:49 -0600

    lab2init

--- 460NLab2/lc3bsim2.c
+++ 460NLab2/lc3bsim2.c
@@ -407,15 +407,226 @@ int main(int argc, char *argv[]) {
 /***************************************************************/
 
 
+int sext(int value, int bits) {
+  int sign_bit = 1 << (bits - 1); // msb mask based on number of bits
+  if (value & sign_bit) {
+    // if negative, sext to 32 bits
+    return value | (0xFFFFFFFF << bits); 
+  }
+  return value; // otherwise it's positive
+}
+
+void setCC(int result) {
+  // clear all condition codes first
+  NEXT_LATCHES.N = 0;
+  NEXT_LATCHES.Z = 0;
+  NEXT_LATCHES.P = 0;
+
+  result = Low16bits(result);
+
+  if (result == 0) {
+    NEXT_LATCHES.Z = 1;
+  } 
+  else if (result & 0x8000) {  //check 16th bit for 1
+    NEXT_LATCHES.N = 1;
+  } 
+  else {
+    NEXT_LATCHES.P = 1;
+  }
+}
 
 void process_instruction(){
   /*  function: process_instruction
-   *  
-   *    Process one instruction at a time  
-   *       -Fetch one instruction
-   *       -Decode 
-   *       -Execute
-   *       -Update NEXT_LATCHES
-   */     
+   
+Process one instruction at a time
+-Fetch one instruction
+-Decode
+-Execute
+-Update NEXT_LATCHES
+*/
+ //fetch
+int pc = CURRENT_LATCHES.PC; //current state of the processor; has the input data (current address)//pc>>1 eqivalent to pc/2 for 2byte addresses
+int instr = (MEMORY[pc >> 1][1] << 8) | (MEMORY[pc >> 1][0]); //Little Endian -> OR the top/bottom 8 bits together for full instr (MEMORY is array of addresses)
+instr = Low16bits(instr); //take care of the overflow
 
+//decode
+int opcode = (instr >> 12) & 0xF; //isolate the opcode by shifting and masking
+NEXT_LATCHES.PC = Low16bits(pc + 2); //pc incremented for next instruction
+
+//execute
+//list of registers/immediates/labels - Components of instructions after opcode
+int dr, sr1, sr2, baseR, imm5, nzp, PCoffset9, PCoffset11, offset6, amount4, trapvect8, val, address, bit4, bit5;
+switch(opcode){
+  case 0: //BR
+    //isolate nzp
+    nzp = (instr >> 9) & 0x7;
+    //isolate PCoffset9;
+    PCoffset9 = sext(instr & 0x1FF, 9); //sext 9 bits
+    if (((nzp & 0x4) && CURRENT_LATCHES.N) || ((nzp & 0x2) && CURRENT_LATCHES.Z) || ((nzp & 0x1) && CURRENT_LATCHES.P)){
+      //checks if any n z p bits are 1s
+      //yes so branch is taken, need to update pc
+      NEXT_LATCHES.PC = Low16bits(NEXT_LATCHES.PC + (PCoffset9 << 1)); //pc = nextpc +2*offset
+    }
+    break; //no branch NEXT_LATCHES.PC is just pc+2
+
+  case 1: //ADD
+    dr  = (instr >> 9) & 0x7; // right shift and get as 3 least sig bits
+    sr1 = (instr >> 6) & 0x7; // right shift
+    
+    if (instr & 0x20) { // isolate steering bit, if 1/0
+      imm5 = sext(instr & 0x1F, 5); 
+      // ex) ADD R1, R2, #5
+      NEXT_LATCHES.REGS[dr] = Low16bits(CURRENT_LATCHES.REGS[sr1] + imm5); // value in sr1 register and then add 5
+    } else { // add registers
+      sr2 = instr & 0x7;
+      // ex) ADD R1, R2, R3
+      NEXT_LATCHES.REGS[dr] = Low16bits(CURRENT_LATCHES.REGS[sr1] + CURRENT_LATCHES.REGS[sr2]); // value in sr1 + sr2
+    }
+    setCC(NEXT_LATCHES.REGS[dr]); //set condition code bits
+    break;
+
+  case 5: // AND
+    dr  = (instr >> 9) & 0x7; // right shift and get as 3 least sig bits
+    sr1 = (instr >> 6) & 0x7; // right shift
+    
+    if (instr & 0x20) { // isolate steering bit, if 1/0
+      imm5 = sext(instr & 0x1F, 5); 
+      // ex) AND R1, R2, #5
+      NEXT_LATCHES.REGS[dr] = Low16bits(CURRENT_LATCHES.REGS[sr1] & imm5); // value in sr1 register and then add 5
+    } else { // add registers
+      sr2 = instr & 0x7;
+      // ex) ADD R1, R2, R3
+      NEXT_LATCHES.REGS[dr] = Low16bits(CURRENT_LATCHES.REGS[sr1] & CURRENT_LATCHES.REGS[sr2]); // value in sr1 + sr2
+    }
+    setCC(NEXT_LATCHES.REGS[dr]); //set condition code bits
+    break;
+
+  case 12: //JMP  or  RET
+    baseR = (instr >> 6) & 0x7; //isolate baseR
+    NEXT_LATCHES.PC = Low16bits(CURRENT_LATCHES.REGS[baseR]); //PC <-- baseR
+    break;
+  
+  case 4: // JSR/JSRR
+    if(instr & 0x800){ //JSR
+      PCoffset11 = sext(instr & 0x7FF, 11);
+      NEXT_LATCHES.REGS[7] = NEXT_LATCHES.PC; // r7 <--  line after
+      NEXT_LATCHES.PC = Low16bits(NEXT_LATCHES.PC + (PCoffset11 << 1)); //pc + offset*2
+    }
+    else{
+      baseR = (instr >> 6) & 0x7;
+      NEXT_LATCHES.REGS[7] = NEXT_LATCHES.PC; 
+      NEXT_LATCHES.PC = Low16bits(CURRENT_LATCHES.REGS[baseR]); 
+    }
+    break;
+
+  case 2: // LDB  -  can be even or odd
+    // DR = SEXT(mem[baseR + SEXT(boffset6)]) ; setcc()
+    dr = (instr >> 9) & 0x7;
+    baseR = (instr >> 6) & 0x7;
+    offset6 = sext(instr & 0x3F, 6);
+    //base + offset
+    address = CURRENT_LATCHES.REGS[baseR] + offset6; //dont need to multiple by 2 bc reading bytes
+    if (address & 1){
+      val = MEMORY[address >> 1][1];  // divide address by 2 for row, grab high byte
+    }
+    else{
+      val = MEMORY[address >> 1][0]; //low byte
+    }
+    NEXT_LATCHES.REGS[dr] = Low16bits(sext(val, 8)); // load into destination register
+    setCC(NEXT_LATCHES.REGS[dr]);
+    break; 
+
+  case 6: // LDW
+    // DR = MEM[BaseR + LSHF(SEXT(offset6),1)] ; setcc()
+    dr = (instr >> 9) & 0x7;
+    baseR = (instr >> 6) & 0x7;
+    offset6 = sext(instr & 0x3F, 6);
+    address = CURRENT_LATCHES.REGS[baseR] + (offset6 << 1);
+    val = (MEMORY[address >> 1][1] << 8) | (MEMORY[address >> 1][0]); //combine two 8 bit numbers to 16 bit little endian
+    NEXT_LATCHES.REGS[dr] = Low16bits(val);
+    setCC(NEXT_LATCHES.REGS[dr]);
+    break; 
+
+  case 14: //LEA
+    // DR = PC + LSHF(SEXT(PCoffset9),1);
+    dr = (instr >> 9) & 0x7;
+    PCoffset9 = sext(instr & 0x1FF, 9);
+    NEXT_LATCHES.REGS[dr] = Low16bits(NEXT_LATCHES.PC + (PCoffset9 << 1));
+    break;
+
+  case 9:  //NOT or XOR. XOR 11111 (xor -1) is the same as NOT
+    // NOT : DR = NOT(SR); setcc() - same as xor imm5 11111 - steering bit both 1
+    dr = (instr >> 9) & 0x7;
+    sr1 = (instr >> 6) & 0x7;
+    if(instr &0x20){  // DR = SR1 XOR SEXT(imm5); setcc()
+      imm5 = sext(instr & 0x1F, 5);
+      NEXT_LATCHES.REGS[dr] = Low16bits(CURRENT_LATCHES.REGS[sr1] ^ imm5);
+
+    }else{  //DR = SR1 XOR SR2
+      sr2 = instr & 0x7;
+      NEXT_LATCHES.REGS[dr] = Low16bits(CURRENT_LATCHES.REGS[sr1] ^ CURRENT_LATCHES.REGS[sr2]); 
+    }
+    setCC(NEXT_LATCHES.REGS[dr]);
+    break;
+
+  case 13:  //SHF
+    bit4 = (instr >> 4) & 0x01; 
+    bit5 = (instr >> 5) & 0x01;
+    dr = (instr >> 9) & 0x7;
+    sr1 = (instr >> 6) & 0x7;
+    val = CURRENT_LATCHES.REGS[sr1];
+    amount4 = instr & 0xF;
+    if (bit4 == 0){
+      // DR = LSHF(SR, amount4)
+      NEXT_LATCHES.REGS[dr] = Low16bits(val << amount4);
+    }
+    else{
+      if(bit5 == 0){
+        // DR = RSHF(SR, amount4, 0)
+        NEXT_LATCHES.REGS[dr] = Low16bits(val >> amount4);
+      }
+      else{
+        // DR = RSHF(SR, amount4, SR[15])  sr15 shifted into vacant
+        NEXT_LATCHES.REGS[dr] = (Low16bits(sext(val, 16) >> amount4));
+      }
+    }
+    setCC(NEXT_LATCHES.REGS[dr]);
+    break;
+
+  case 3: // STB
+    // mem[BaseR + SEXT(boffset6)] = SR[7:0]
+    sr1 = (instr >> 9) & 0x7;
+    baseR = (instr >> 6) & 0x7;
+    offset6 = sext(instr & 0x3F, 6);
+    address = Low16bits(CURRENT_LATCHES.REGS[baseR] + offset6); //byte, dont multiple by 2
+    val = CURRENT_LATCHES.REGS[sr1] & 0xFF; //get a byte
+    if(address & 0x1){ //odd address
+      MEMORY[address >> 1][1] = val;
+    }else{  // even address
+      MEMORY[address >> 1][0] = val; 
+    }
+    break;
+
+  case 7:  // STW 
+    // MEM[BaseR + LSHF(SEXT(offset6), 1)]
+    sr1 = (instr >> 9) & 0x7;
+    baseR = (instr >> 6) & 0x7;
+    offset6 = sext(instr & 0x3F, 6);
+    address = Low16bits(CURRENT_LATCHES.REGS[baseR] + (offset6<<1)); //word, multiple by 2
+    val = CURRENT_LATCHES.REGS[sr1];
+    MEMORY[address >> 1][1] = (val >> 8) & 0xFF;
+    MEMORY[address >> 1][0] = val & 0xFF;
+    break;
+
+  case 15: //TRAP
+    // R7 = PC+;  PC = MEM[LSHF(ZEXT(trapvect8), 1)]
+    trapvect8 = instr & 0xFF; // 16 bits only, technically zero extended past
+    NEXT_LATCHES.REGS[7] = NEXT_LATCHES.PC;
+    address = (trapvect8 << 1); 
+    NEXT_LATCHES.PC = Low16bits((MEMORY[address >> 1][1] << 8) | MEMORY[address >> 1][0]);
+    break;
+
+  default: 
+    exit(1);
+  }
 }
