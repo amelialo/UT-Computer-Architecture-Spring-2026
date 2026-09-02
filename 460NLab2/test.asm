76c67086505ab83a06ccefbaa92c47a0bea4d2ad
amelialo <amelialo@utexas.edu>
2026-09-02 01:42:42 -0500

    remove files

--- 460NLab2/test.asm
+++
@@ -1,5 +0,0 @@
-.ORIG x3000       ; Program starts at x3000
-ADD R1, R1, #5    ; R1 should become 5
-ADD R2, R1, #2    ; R2 should become 7 (5 + 2)
-HALT              ; TRAP x25 (Stops simulator)
-.END
